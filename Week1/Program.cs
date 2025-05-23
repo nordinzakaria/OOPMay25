45b2f2f1c43440659dfb1fab8804ce29b247e7c2
nordinzakaria <nordinzakaria@gmail.com>
2025-05-23 23:33:25 +0800

    Create Program.cs
    week 1 updated

---
+++ Week1/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection.Metadata;
+
+Console.WriteLine("Hello, World!");
+
+float[] amount = new float[5];
+string[] description = new string[5];
+int[] day = new int[5], 
+        month = new int[5], 
+        year = new int[5];
+
+int option;
+
+int sz = 0;
+
+while (true)
+{
+    Console.WriteLine("Options: 1) Read transaction " +
+                    " 2) Print transactions " +
+                    " 3) Calculate average");
+    option = int.Parse(Console.ReadLine());
+
+
+    if (option == 0)
+    {
+        description[sz] = Console.ReadLine();
+        amount[sz] = float.Parse(Console.ReadLine());
+        day[sz] = int.Parse(Console.ReadLine());
+        Console.WriteLine(" product sold: " + description);
+        Console.WriteLine("     amount: " + amount);
+
+        sz++;
+    }
+    else if (option == 1)
+    {
+
+
+    }
+
+}
