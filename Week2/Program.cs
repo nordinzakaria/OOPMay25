3a7aa88f3cc112528f0c32ee647d4b286553e43f
nordinzakaria <nordinzakaria@gmail.com>
2025-05-24 12:21:24 +0800

    update the coed
    updated

--- Week2/Program.cs
+++ Week2/Program.cs
@@ -19,7 +19,7 @@ int sz = 0;
 while (true) // infinite loop
 {
     Console.WriteLine("Options: 1) Read transaction " +
-                    " 2) Print transactions " +
+                    " 2) Print transactions " + // complete this option
                     " 3) Calculate average");
     option = int.Parse(Console.ReadLine());
 
@@ -27,17 +27,26 @@ while (true) // infinite loop
     if (option == 1)
     {
         Console.WriteLine("Enter desc, amount and day");
-        description[sz] = Console.ReadLine();
-        amount[sz] = float.Parse(Console.ReadLine());
-        day[sz] = int.Parse(Console.ReadLine());
-        Console.WriteLine(" product sold: " + description);
-        Console.WriteLine("     amount: " + amount);
+
+        // arrayname [ index ] . field 
+        sales[sz].description = Console.ReadLine();
+        sales[sz].amount = float.Parse(Console.ReadLine());
+        sales[sz].day = int.Parse(Console.ReadLine());
+        Console.WriteLine(" product sold: " + sales[sz].description);
+        Console.WriteLine("     amount: " + sales[sz].amount);
 
         sz++;
     }
-    else if (option == 1)
-    {
-
+    else if (option == 2)
+    {   // complete this
+        for (int i = 0; i < sz; i++) {
+        {
+            Console.WriteLine("----|" + "Item: " + (i + 1) + "|----");
+            Console.WriteLine("Item Description: " + sales[i].description);
+            Console.WriteLine("Item Amount: " + sales[i].amount);
+            Console.WriteLine("Item Date: " + sales[i].day + "/" + sales[i].month 
+                                            + "/" + sales[i].year);
+        }
 
     }
 
