f6a2084c2f2806b1c0762694c97ed7e49c30f9f7
nordinzakaria <nordinzakaria@gmail.com>
2025-05-26 15:27:14 +0800

    update Character

---
+++ Week3/Character.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // fields
+    private Rigidbody rb;
+
+
+    // methods
+
+    // is called before the first frame update
+    void Start()    // called once
+    {
+        Debug.Log("Starting ");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()   // called every frame
+    {
+        Debug.Log("Updating ");
+
+            // Input: preexisting class,
+            // GetKey: a method/function inside Input
+        if (Input.GetKey("up"))
+        {
+            MoveFront();
+        }
+    }
+
+    void MoveBack()
+    {
+
+    }
+
+    void MoveFront()
+    {
+        rb.velocity = new Vector3(0, 0, -1);
+    }
+
+    // add codes for moving in whatever direction
+}
