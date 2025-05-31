fea4e85a1657f3b666e7adf71cef4ed2890826fa
nordinzakaria <nordinzakaria@gmail.com>
2025-05-31 12:52:06 +0800

    sq and rect

---
+++ Week4/VectorGraphics/Rectangle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Rectangle
+
+    {
+
+        public float width;
+
+        public float height;
+
+        public float posX, posY;
+
+        public Rectangle()
+
+        {
+
+            width = 1;
+
+            height = 1;
+
+            posX = 0;
+
+            posY = 0;
+
+        }
+        public Rectangle(float width, float height, float posX, float posY)
+
+        {
+
+            this.width = width;
+
+            this.height = height;
+
+            this.posX = posX;
+
+            this.posY = posY;
+
+        }
+
+        public float Area()
+
+        {
+
+            return width * height;
+
+        }
+        public float Perimeter()
+
+        {
+
+            return 2 * (width + height);
+
+        }
+
+    }
+
+}
---
+++ Week4/VectorGraphics/Square.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Square
+    {
+    public double Side;
+    public float posX, posY;
+
+    public Square()
+    {
+        Side = 1;
+        posX = 0;
+        posY = 0;
+    }
+
+    public Square(double side, float posX, float posY)
+    {
+        Side = side;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public float Area()
+    {
+        return 0;
+    }
+
+    public float Perimeter()
+    {
+        return 0;
+    }
+
+}
