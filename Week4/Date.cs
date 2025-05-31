32972a01d0b5d72f30634223a11d811ee026c4c6
nordinzakaria <nordinzakaria@gmail.com>
2025-05-31 11:56:06 +0800

    Update Date.cs

--- Week4/Date.cs
+++ Week4/Date.cs
@@ -10,8 +10,19 @@ public class Date
     public int month;
     public int day;
 
-    public Date() 
-    { 
-        
+    public Date() // constructor
+    {
+        day = 1;
+        month = 1;
+        year = 2000;
+        Console.WriteLine("In constructor; initialized values");
     }
-  }
+
+    public Date(int day_, int month_, int year_) // constructor ver 2
+    {   // make use of the parameters to initialize the attributes
+        day = day_;
+        month = month_;
+        year = year_;
+        Console.WriteLine("In constructor ver 2; initialized values");
+    }
+}
