07639f230e3cc110f253cec2c09d708c22854430
nordinzakaria <nordinzakaria@gmail.com>
2025-06-23 15:58:18 +0800

    with inheritance

---
+++ Week7/Shapes/ColoredCircle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Shapes
+{
+                            // INHERITANCE
+                            // - whatever public, protected is accessible in here
+    internal class ColoredCircle : Circle
+    {
+        public Color Color {  get; set; }
+
+        public ColoredCircle() { }
+
+        public void Draw(Graphics g1)
+        {
+            Pen p = new Pen(this.Color, 2);
+            g1.DrawEllipse(p, this.X, this.Y, this.Radius, this.Radius);
+        }
+    }
+}
+}
