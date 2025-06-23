0c2148e76566180481f2d9b9cf5881134377ba97
nordinzakaria <nordinzakaria@gmail.com>
2025-06-23 14:22:25 +0800

    Week7

---
+++ Week7/Rectangle - Copy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Shapes
+{
+    internal class Rectangle
+    {
+        public float X {  get; set; }
+        public float Y {/* BLANK */}
+
+        public float Width { get; set; }
+        public float Height { get; set; }
+
+        public Rectangle() {  Width = 0; Height = 0;}
+
+        public Rectangle(float x, float y, float width, float height)
+        {
+            /* BLANK */
+        }
+
+        public float CalcArea(/* BLANK */)
+        {
+            /* BLANK */
+        }
+
+        public /* BLANK */ calcPerimeter(/* BLANK */)
+        {
+            /* BLANK */
+        }
+
+    }
+}
