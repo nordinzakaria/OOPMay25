40d02965b96c3c2cd8e707972dbb3d7a1ed01476
nordinzakaria <nordinzakaria@gmail.com>
2025-06-30 14:26:30 +0800

    Create Character.cs

---
+++ Week8/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Character
+    {
+        public Position Pos {  get; set; }
+
+        public Character()
+        {
+            Pos = new Position();
+        }
+
+        public void Move()
+        {
+            Pos.X += 10;
+            Pos.Y += 10;
+        }
+
+    }
+}
