75002ab1fd5b9682282cdca257ab327d738b4af6
nordinzakaria <nordinzakaria@gmail.com>
2025-07-07 15:42:06 +0800

    Bullet

---
+++ Week9/Asset/ColoredCharCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ColoredCharCtrl : CharCtrl
+    {
+
+        // Use this for initialization
+        protected void Start()
+        {
+            base.Start();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            base.Update();
+
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                TurnRed();
+            }
+        }
+
+        protected void TurnRed()
+        {
+                                                       // red is a *static* attribute 
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+
+
+    }
+}
